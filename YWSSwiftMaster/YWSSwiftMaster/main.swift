7813149f6d09ad5af3c544cd4fdd6d8b347dd3c3
_GGDol <bohg6799@naver.com>
2025-03-21 09:24:12 +0900

    refactor: 디렉토리 구분 및 주석문 추가
    - 디렉토리 구분
    - 주석문 추가

--- YWSSwiftMaster/YWSSwiftMaster/main.swift
+++ YWSSwiftMaster/YWSSwiftMaster/main.swift
@@ -5,23 +5,12 @@
 //  Created by 양원식 on 3/21/25.
 //
 
-import Foundation
 
-// 1. 두 개의 Int 값을 받아 문자열을 반환하는 클로저
-let sum: (Int, Int) -> String = { a, b in
-    return "두 수의 합은 \(a + b) 입니다"
-}
+//MARK: - 필수 문제 1
 
-var num: Int = 3
 
 // 2. sum을 호출
-print(sum(3, 5)) // 두 수의 합은 8 입니다.
-
-// 3. 클로저를 파라미터로 받고 반환값이 없는 함수
-func calculate(operation: (Int, Int) -> String) {
-    print(operation(4, 7))
-}
+print(sum(3, 5))
 
 calculate(operation: sum)
-
 
---
+++ YWSSwiftMaster/YWSSwiftMaster/필수문제 1/Problem1.swift
@@ -0,0 +1,25 @@
+//
+//  Problem1.swift
+//  YWSSwiftMaster
+//
+//  Created by 양원식 on 3/21/25.
+//
+
+import Foundation
+
+//MARK: - 필수 문제 1
+
+// 1. 두 개의 Int 값을 받아 문자열을 반환하는 클로저
+let sum: (Int, Int) -> String = { a, b in
+    return "두 수의 합은 \(a + b) 입니다"
+}
+
+var num: Int = 3
+
+
+// 3. 클로저를 파라미터로 받고 반환값이 없는 함수
+func calculate(operation: (Int, Int) -> String) {
+    print(operation(4, 7))
+}
+
+
