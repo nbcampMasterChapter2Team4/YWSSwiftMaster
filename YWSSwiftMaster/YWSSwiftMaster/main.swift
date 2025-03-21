e4f8db17cb9e7150d1857ce4aae3da5a488c12fb
_GGDol <bohg6799@naver.com>
2025-03-21 09:18:34 +0900

    feat: 필수과제 1번 구현

--- YWSSwiftMaster/YWSSwiftMaster/main.swift
+++ YWSSwiftMaster/YWSSwiftMaster/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
+// 1. 두 개의 Int 값을 받아 문자열을 반환하는 클로저
+let sum: (Int, Int) -> String = { a, b in
+    return "두 수의 합은 \(a + b) 입니다"
+}
+
+var num: Int = 3
+
+// 2. sum을 호출
+print(sum(3, 5)) // 두 수의 합은 8 입니다.
+
+// 3. 클로저를 파라미터로 받고 반환값이 없는 함수
+func calculate(operation: (Int, Int) -> String) {
+    print(operation(4, 7))
+}
+
+calculate(operation: sum)
+
 
